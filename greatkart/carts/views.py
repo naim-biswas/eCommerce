5a48b9127f5f5df86783a40961b032dfc969921b
Naim Biswas <naimbiswas@yahoo.com>
2021-10-28 15:32:22 +0600

    second

--- greatkart/carts/views.py
+++ greatkart/carts/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+
+
+
 def cart(request):
     return render(request,'store/cart.html')
